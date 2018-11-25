cfde2ebe2c2f6149b75e5e487da3d036269e6fcc
Amanda Viceconte <amanda.viceconte@gmail.com>
2018-11-25 13:05:11 -0200

    Design aprimorado

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   console.log (`it's working!`); 
   $('#article1').append(`<button type="button" id="btn1">Click Me!</button>`);
+
+  // Ação de clicar no botão login da página Cadastro/Login
+  $('#log-bt').click(function() {
+    $('#logcad-desc').text("Login");
+    $(this).css('font-weight', 'bold');
+  });
+
+  // Ação de clicar no botão cadastro da página Cadastro/Login
+  $('#cad-bt').click(function() {
+    $('#logcad-desc').text("Cadastro");
+    $(this).css('font-weight', 'bold');
+    $('.logcad-form').append(`<div>OI TUDO BEM?</div>`);
+  });
+
   
 });
+
